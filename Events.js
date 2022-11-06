c6d59381924ec6d2cdd9b69c528b751db653845e
Astonisthebest <astonbratley3@gmail.com>
2022-11-06 20:41:38 +1000

    Update Events.js

--- Events.js
+++ Events.js
@@ -6,5 +6,5 @@ function getready() {
 
 window.addEventListener("gameblock",answer);
 function answer() {
-    console.log(kahoot.answer[kahoot.current_question]);
+    console.log(kahoot.answers[kahoot.current_question]);
 }
