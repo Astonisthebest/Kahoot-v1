c8b7d180e7299aa931562601b7d2fa43f54fd3ab
Astonisthebest <astonbratley3@gmail.com>
2022-11-06 13:59:37 +1000

    Event Tools
    Tools to help with main script

---
+++ EventTools.js
@@ -0,0 +1,37 @@
+(() => {
+
+        let previousUrl = '';
+
+        const observer = new MutationObserver(function(mutations) {
+
+            if (location.href !== previousUrl) {
+
+                previousUrl = location.href;
+
+                window.dispatchEvent(new Event("urlchange"));
+
+            }
+
+        });
+        
+        const config = {
+
+            subtree: true,
+
+            childList: true
+
+        };
+
+        observer.observe(document, config);
+
+    })();
+
+    window.addEventListener("urlchange", function() {
+        
+        var url = location.href;
+
+        var sub_url = url.slice(url.indexOf("kahoot.it/") + 10,url.length);
+
+        window.dispatchEvent(sub_url);
+
+    });
