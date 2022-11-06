122c69e3d4da8e576012f18648529889284586b5
Astonisthebest <astonbratley3@gmail.com>
2022-11-06 20:21:04 +1000

    Update Events.js

--- Events.js
+++ Events.js
@@ -3,3 +3,8 @@ function getready() {
     kahoot.current_question = JSON.parse(localStorage.getItem("kahoot-game_session")).questionNumber;
     console.log("Question " + kahoot.current_question)
 }
+
+window.addEventListener("gameblock",answer);
+function answer() {
+    console.log(kahoot.answer[kahoot.current_question]);
+}
