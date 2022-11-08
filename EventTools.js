4bb68e259954b6e2f328d5c1a08129f9d5f1d003
Astonisthebest <astonbratley3@gmail.com>
2022-11-08 16:17:43 +1000

    Update EventTools.js

--- EventTools.js
+++ EventTools.js
@@ -1,39 +1,77 @@
+//when script injects immediately send inject event;
+const injectEvent = new Event("inject");
+window.dispatchEvent(injectEvent);
+
+
 (() => {
 
-        let previousUrl = '';
+    let previousUrl = '';
 
-        const observer = new MutationObserver(function(mutations) {
+    const observer = new MutationObserver(function(mutations) {
 
-            if (location.href !== previousUrl) {
+        if (location.href !== previousUrl) {
 
-                previousUrl = location.href;
+            previousUrl = location.href;
 
-                window.dispatchEvent(new Event("urlchange"));
+            window.dispatchEvent(new Event("urlchange"));
 
-            }
+        }
 
-        });
+    });
         
-        const config = {
+    const config = {
 
-            subtree: true,
+        subtree: true,
 
-            childList: true
+        childList: true
 
-        };
+    };
 
-        observer.observe(document, config);
+    observer.observe(document, config);
 
-    })();
+})();
 
-    window.addEventListener("urlchange", function() {
+window.addEventListener("urlchange", function() {
         
         var url = location.href;
 
         var sub_url = url.slice(url.indexOf("kahoot.it/") + 10,url.length);
 
-        console.log("Event triggered : " + sub_url);
+        var event = sub_url == "join" ? "PinEnter" : sub_url == "instructions" ? "QuizJoin" : sub_url == "start" ? "QuizStart" : sub_url == "getready" ? "BeforeQuestion" : sub_url == "gameblock" ? "QuestionStart" : sub_url == "answer/result" ? "AfterQuestion": sub_url == "answer/sent" ? "QuestionAnswer" : 1;
 
-        window.dispatchEvent(new Event(sub_url));
+        var details = event == "QuizJoin" ? () => {
+            var data = {
+                PlayerName : JSON.parse(localStorage.getItem("kahoot-game_session")).playerName,
+                QuizPin : JSON.parse(localStorage.getItem("kahoot-game_session")).pin
+            }
+            return data;
+        } : event == "BeforeQuestion" ? () => {
+            var data = {
+                QuestionText : document.querySelector("[data-functional-selector]='question-block-title'").children[0].children[0]
+            }
+            return data
+        } : event == "QuestionStart" ? () => {
+            var data = {
+                Answer1 : {
+                    button : document.querySelector("[data-functional-selector]='answer-0'"),
+                    text : document.querySelector("[data-functional-selector]='question-choice-text-0'")
+                },
+                Answer2 : {
+                    button : document.querySelector("[data-functional-selector]='answer-1'"),
+                    text : document.querySelector("[data-functional-selector]='question-choice-text-1'")
+                },
+                Answer3 : {
+                    button : document.querySelector("[data-functional-selector]='answer-1'"),
+                    text : document.querySelector("[data-functional-selector]='question-choice-text-1'")
+                },
+                Answer4 : {
+                    button : document.querySelector("[data-functional-selector]='answer-1'"),
+                    text : document.querySelector("[data-functional-selector]='question-choice-text-1'")
+                }
+            }
+            return data;
+        } : false;
+        document.dispatchEvent(new CustomEvent(event),details);
+        console.log("event:" + event + "\ndata :",data)
+});
 
-    });
